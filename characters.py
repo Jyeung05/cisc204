651afc8c5db514ed63c7c9416d9900b6e9bd8b65
Brian Constandes <brian.constandes@gmail.com>
2024-10-30 13:13:29 -0400

    finished character dictionary

--- characters.py
+++ characters.py
@@ -1,7 +1,13 @@
 
 characters = {
-    'Alex':['male', 'black_hair', 'has_hair', 'has_facial_hair', 'no_accessories', 'is_smiling'],
-    'Alfred': ['male', 'orange_hair', 'has_hair']
+    'Alex':['male', 'black_hair', 'hair', 'facial_hair', 'accessories', 'smiling'],
+    'Alfred': ['male', 'orange_hair', 'hair', 'has_facial_hair', 'no_accessories', 'not_smiling'],
+    'Anita' : ['female', 'blonde_hair', 'hair', 'no_facial_hair', 'accessories', 'smiling'],
+    'Anne' : ['female', 'black_hair', 'hair', 'no_facial_hair', 'accessories', 'not_smiling'],
+    'Bernard' : ['male', 'brown_hair', 'hair', 'no_facial_hair', 'accessories', 'not_smiling'],
+    'Bill' : ['male', 'orange_hair', 'no_hair', 'facial_hair', 'no_accessories', 'smiling'],
+    'Charles' : ['male', 'blonde_hair', 'hair', 'facial_hair', 'no_accessories', 'smiling'],
+    'Claire' : ['female', 'blonde_hair', 'hair' , 'no_facial_hair', 'accessories', 'smiling']
 }
 
 
