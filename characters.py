0e34375c1c753f1f4f48ce55e4d0808eb2b3ce0b
Brian Constandes <brian.constandes@gmail.com>
2024-10-30 15:00:39 -0400

    added all 24 characters to dictionary with 6 traits

--- characters.py
+++ characters.py
@@ -1,14 +1,31 @@
 
 characters = {
-    'Alex':['male', 'black_hair', 'hair', 'facial_hair', 'accessories', 'smiling'],
-    'Alfred': ['male', 'orange_hair', 'hair', 'has_facial_hair', 'no_accessories', 'not_smiling'],
-    'Anita' : ['female', 'blonde_hair', 'hair', 'no_facial_hair', 'accessories', 'smiling'],
-    'Anne' : ['female', 'black_hair', 'hair', 'no_facial_hair', 'accessories', 'not_smiling'],
-    'Bernard' : ['male', 'brown_hair', 'hair', 'no_facial_hair', 'accessories', 'not_smiling'],
-    'Bill' : ['male', 'orange_hair', 'no_hair', 'facial_hair', 'no_accessories', 'smiling'],
-    'Charles' : ['male', 'blonde_hair', 'hair', 'facial_hair', 'no_accessories', 'smiling'],
-    'Claire' : ['female', 'blonde_hair', 'hair' , 'no_facial_hair', 'accessories', 'smiling']
+    'Alex':['male', 'black_hair', 'hair', 'facial_hair', 'accessories', 'smiling', 'no_glasses'],
+    'Alfred': ['male', 'orange_hair', 'hair', 'has_facial_hair', 'no_accessories', 'not_smiling', 'no_glasses'],
+    'Anita' : ['female', 'blonde_hair', 'hair', 'no_facial_hair', 'accessories', 'smiling', 'no_glasses'],
+    'Anne' : ['female', 'black_hair', 'hair', 'no_facial_hair', 'accessories', 'not_smiling', 'no_glasses'],
+    'Bernard' : ['male', 'brown_hair', 'hair', 'no_facial_hair', 'accessories', 'not_smiling', 'no_glasses'],
+    'Bill' : ['male', 'orange_hair', 'no_hair', 'facial_hair', 'no_accessories', 'smiling', 'no_glasses'],
+    'Charles' : ['male', 'blonde_hair', 'hair', 'facial_hair', 'no_accessories', 'smiling', 'no_glasses'],
+    'Claire' : ['female', 'blonde_hair', 'hair' , 'no_facial_hair', 'accessories', 'smiling', 'glasses'], 
+    'David': ['male', 'blonde_hair', 'hair', 'facial_hair', 'no_accessories', 'smiling', 'no_glasses'], 
+    'Eric': ['male', 'blonde_hair', 'hair', 'no_facial_hair', 'accessories', 'smiling', 'no_glasses', 'no_glasses'], 
+    'Frans': ['male', 'orange_hair', 'hair', 'no_facial_hair', 'no_accessories', 'smiling', 'no_glasses'], 
+    'George': ['male', 'white_hair', 'hair', 'no_facial_hair', 'accessories', 'not_smiling', 'no_glasses'], 
+    'Herman': ['male', 'orange_hair', 'no_hair', 'no_facial_hair', 'no_accessories', 'not_smiling', 'no_glasses'], 
+    'Joe': ['male', 'blonde_hair', 'hair', 'no_facial_hair', 'no_accessories', 'not_smiling', 'glasses'], 
+    'Maria': ['female', 'brown_hair', 'hair', 'no_facial_hair', 'accessories', 'smiling', 'no_glasses'], 
+    'Max': ['male', 'black_hair', 'hair', 'facial_hair', 'no_accessories', 'smiling', 'no_glasses'], 
+    'Paul': ['male', 'white_hair', 'hair', 'no_facial_hair', 'no_accessories', 'smiling', 'glasses'], 
+    'Peter': ['male', 'white_hair', 'hair', 'no_facial_hair', 'no_accessories', 'smiling', 'no_glasses'], 
+    'Philip': ['male', 'black_hair', 'hair', 'facial_hair', 'no_accessories', 'smiling', 'no_glasses'], 
+    'Richard': ['male', 'brown_hair', 'no_hair', 'facial_hair', 'no_accessories', 'smiling' 'no_glasses'], 
+    'Robert': ['male', 'brown_hair', 'hair', 'no_facial_hair', 'no_accessories', 'not_smiling', 'no_glasses'], 
+    'Sam': ['male', 'white_hair', 'no_hair', 'no_facial_hair', 'no_accessories', 'smiling', 'glasses'], 
+    'Susan': ['female', 'white_hair', 'hair', 'no_facial_hair', 'no_accessories', 'smiling', 'no_glasses'], 
+    'Tom': ['male', 'black_hair', 'no_hair', 'no_facial_hair', 'no_accessories', 'smiling', 'glasses']
 }
+
 
 
     
