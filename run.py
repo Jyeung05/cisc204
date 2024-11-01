9e9b19b3551556841374a7ab0eb819fd8cf78f15
Jeffrey Yeung <jyeungprivate@gmail.com>
2024-10-31 23:26:21 -0400

    draft complete

--- run.py
+++ run.py
@@ -2,7 +2,7 @@
 from bauhaus import Encoding, proposition, constraint
 from bauhaus.utils import count_solutions, likelihood
 from characters import characters
-import random
+from characters import questions
 
 # These two lines make sure a faster SAT solver is used.
 from nnf import config
@@ -12,26 +12,11 @@ config.sat_backend = "kissat"
 E = Encoding()
 
 # variables to store the chosen characters
-playerCharacter = None
-botCharacter = None
-
-#starts the game
-def begin():
-    for name in characters:
-        characters[name] = {
-            'traits': characters[name],
-            'is_up': True  # Initialize all characters as 'up'
-        }
-# Now, characters dictionary looks like this:
-# {
-#     'Alex': {'traits': ['blond hair', 'male', 'glasses'], 'is_up': True},
-#     'Beth': {'traits': ['brown hair', 'female', 'hat'], 'is_up': True},
-#     # ...
-# }
+character = None
 def remainingCharacters():
     remaining = []
     for i in range(len(characters)):
-        if characters[i]['is_up'] == true:
+        if characters[i]['is_up'] == True:
             remaining.append(characters[i])
     return remaining
         
@@ -56,7 +41,8 @@ class CheckTrait:
         assert a in questions
         assert b in questions
         self.person = characters
-        self.trait 
+        self.trait1 = guess1
+        self.trait2 = guess2
 
     def _prop_name(self):
         return f"A.{self.data}"
@@ -70,19 +56,25 @@ class GuessCharacter:
         return f"A.{self.data}"
 
 
-# Different classes for propositions are useful because this allows for more dynamic constraint creation
-# for propositions within that class. For example, you can enforce that "at least one" of the propositions
-# that are instances of this class must be true by using a @constraint decorator.
-# other options include: at most one, exactly one, at most k, and implies all.
-# For a complete module reference, see https://bauhaus.readthedocs.io/en/latest/bauhaus.html
-
-
 # Build an example full theory for your setting and return it.
 #
 #  There should be at least 10 variables, and a sufficiently large formula to describe it (>50 operators).
 #  This restriction is fairly minimal, and if there is any concern, reach out to the teaching staff to clarify
 #  what the expectations are.
 def example_theory():
+    # For any two distinct characters c1 and c2, and all traits t, no character can have all the same traits. 
+    for c1 in characters:
+        for c2 in characters:
+            if c1 in characters and c2 in characters:
+                if characters[c1] == characters[c2]: # not sure if i am properly creating this constraint
+                    E.add_constraint(~(CheckTrait&~CheckTrait))
+
+    # For two distinct characters c1 and c2, each question posed by a player for a given trait t must distinguish 
+    # at least one character c1 from the other c2. Only for characters that are up.
+
+
+    #For any character c and trait t, if a character has a trait then they cannot simultaneously have its negation.
+
 
     return E
 
@@ -93,10 +85,11 @@ if __name__ == "__main__":
     T = T.compile()
     # After compilation (and only after), you can check some of the properties
     # of your model:
+    
     begin()
     print(remainingCharacters())
-    playerCharacter = input("Choose your character (capitalise first letter): ")
-    while playerCharacter not in characters:
-        playerCharacter = input("Invalid character, please try again: ")
-    botCharacter = characters[random.randint(1,24)]
+    character = input("Choose your character (capitalise first letter): ")
+    while character not in characters:
+        character = input("Invalid character, please try again: ")
+
     
